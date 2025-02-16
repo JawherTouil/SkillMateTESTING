32f9a1d0348a4a651f8b4fe1996e2a9bb20191d0
jawher <touil.jawher@esprit.tn>
2025-02-16 20:28:17 +0100

    login and encrypted password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,29 @@
 const User = require('../models/User');
-//test1
-//test2
-//test3
-//test4
-//test5
-//test6
-//test7
-//test8
-//test9
-//test10
-//test11aa
+
+async function login(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "User not found" });
+        }
+
+        // Compare password
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        // Successful login
+        res.status(200).json({ message: "Login successful", user: { id: user._id, username: user.username, email: user.email, role: user.role } });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
 async function add(req, res) {
     try {
       const user = new User(req.body);
@@ -65,6 +79,14 @@ async function remove(req, res) {
         res.status(500).send({ error: error.toString() });
     }
 }
+async function searchByUsername(req, res) {
+    try {
+        const users = await User.find({ username: new RegExp(req.params.username, 'i') });
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(500).send({ error: error.toString() });
+    }
+}
 
-module.exports = {add,remove,update,getAll,getById};
+module.exports = {add,remove,update,getAll,getById,searchByUsername,login};
 
